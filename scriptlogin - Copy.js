5b7aba18c5ad83bb22d4372d5d3c12f8716becb1
AhmadRizqi0505 <101494453+AhmadRizqi0505@users.noreply.github.com>
2025-11-13 10:57:29 +0700

    Add files via upload

---
+++ scriptlogin - Copy.js
@@ -0,0 +1,23 @@
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // Contoh username & password yang valid
+  if (username === "Citra" && password === "Citra2025@@") {
+  const btn = document.querySelector(".button");
+	btn.classList.add("button--loading");
+	
+	  setTimeout(() => {
+		  window.location.href = "index.html";
+		}, 2000);
+	  } else {
+		alert("Username atau password salah!");
+	
+	window.addEventListener("pageshow", function(event) {
+    const button = document.querySelector(".button");
+    button.classList.remove("button--loading");
+  });
+  }
+});
